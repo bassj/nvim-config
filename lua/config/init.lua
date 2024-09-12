dc68fe1008a04ce89a843651d505b038ad6b12f0
Jack Bass <jbass2000@gmail.com>
2024-09-12 17:40:38 -0400

    Initial Commit

---
+++ lua/config/init.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.syntax = 'on'
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.foldmethod = 'indent'
+
+vim.opt.background = 'dark'
+
+require("config.lazy")
