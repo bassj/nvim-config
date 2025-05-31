5ef6607249149f0bc8d82f4a2c885522f618bf74
Jack Bass <jbass2000@gmail.com>
2025-05-31 16:54:49 -0400

    Added Plugins, Fixed telescope issue

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,12 +5,14 @@ local M = {
         'nvim-telescope/telescope-fzf-native.nvim'
     },
     config = function ()
-        local builtin = require("telescope.builtin")
+        local status, builtin = pcall(require, "telescope.builtin")
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        if status then
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end
     end
 }
 
